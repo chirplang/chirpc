f15b06e962ffb90fb37813932d7978d77f0520e8
schitcrafter <vapor.schitcrafter@gmail.com>
2022-03-13 12:05:15 +0100

    Implement identifiers and function calls Also tests and some other refactoring

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,24 @@
 use std::fmt::{Debug, Error, Formatter};
 
-pub enum Expr {
-    Number(i32),
-    Op(Box<Expr>, Opcode, Box<Expr>),
+pub enum Expr<'a> {
+    Number(Number),
+    Op(Box<Expr<'a>>, Opcode, Box<Expr<'a>>),
+    FunctionCall(FunctionCall<'a>),
     Error,
+}
+
+pub struct FunctionCall<'a> {
+    pub ident: Ident<'a>,
+    pub args: IdentList<'a>,
+}
+
+pub struct IdentList<'a>(pub Vec<Ident<'a>>);
+
+pub struct Ident<'a>(pub &'a str);
+
+pub enum Number {
+    Int(i64),
+    Float(f64),
 }
 
 pub enum Opcode {
@@ -13,14 +28,54 @@ pub enum Opcode {
     Sub,
 }
 
-impl Debug for Expr {
+impl Debug for Expr<'_> {
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
         use self::Expr::*;
         match self {
             Number(n) => write!(fmt, "{:?}", n),
             Op(ref l, op, ref r) => write!(fmt, "({:?} {:?} {:?})", l, op, r),
+            FunctionCall(f) => write!(fmt, "{:?}", f),
             Error => write!(fmt, "error"),
         }
+    }
+}
+
+impl Debug for Number {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::Number::*;
+        match *self {
+            Int(i) => write!(fmt, "{}", i),
+            Float(i) => write!(fmt, "{}", i),
+        }
+    }
+}
+
+impl Debug for FunctionCall<'_> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        write!(fmt, "{:?}{:?}", self.ident, self.args)
+    }
+}
+
+impl Debug for IdentList<'_> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        if self.0.is_empty() {
+            return write!(fmt, "()");
+        }
+        let mut comma_separated = String::new();
+
+        for item in &self.0[0..self.0.len() - 1] {
+            comma_separated.push_str(&format!("{:?}", item));
+            comma_separated.push_str(", ");
+        }
+
+        comma_separated.push_str(&format!("{:?}", self.0[self.0.len() - 1]));
+        write!(fmt, "({})", comma_separated)
+    }
+}
+
+impl Debug for Ident<'_> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        write!(fmt, "{}", self.0)
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -9,41 +9,96 @@ pub enum MainError {
 lalrpop_mod!(pub main_parser);
 pub mod ast;
 
-#[test]
-fn parse_test() {
-    let mut errors = vec![];
-    let expr = main_parser::ExprParser::new()
-        .parse(&mut errors, "22 * 44 + 66")
-        .unwrap();
-    assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
-}
+#[cfg(test)]
+mod test {
+    use std::vec;
 
-#[test]
-fn number_too_big() {
-    let mut errors = vec![];
-    let expr = main_parser::ExprParser::new().parse(&mut errors, "2147483648");
-    assert!(expr.is_err());
-    assert_eq!(
-        expr.unwrap_err(),
-        lalrpop_util::ParseError::User {
-            error: MainError::InputTooBig
-        }
-    );
-}
+    use super::*;
 
-#[test]
-fn parse() {
-    let mut errors = vec![];
-    let expr = main_parser::ExprParser::new()
-        .parse(&mut errors, "3 * * 2 + 7")
-        .unwrap();
-    assert_eq!(&format!("{:?}", expr), "(((3 * error) * 2) + 7)");
-}
+    #[test]
+    fn parse_test() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new()
+            .parse(&mut errors, "22 * 44 + 66")
+            .unwrap();
+        assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
+    }
 
-#[test]
-fn negative_int_literal() {
-    let mut errors = vec![];
-    let expr = main_parser::ExprParser::new().parse(&mut errors, "-200");
-    assert!(expr.is_ok());
-    assert_eq!(&format!("{:?}", expr.unwrap()), "-200");
+    #[test]
+    fn number_too_big() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new().parse(&mut errors, "9223372036854775808");
+        assert!(expr.is_err());
+        assert_eq!(
+            expr.unwrap_err(),
+            lalrpop_util::ParseError::User {
+                error: MainError::InputTooBig
+            }
+        );
+    }
+
+    #[test]
+    fn parse() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new()
+            .parse(&mut errors, "3 * * 2 + 7")
+            .unwrap();
+        assert_eq!(&format!("{:?}", expr), "(((3 * error) * 2) + 7)");
+    }
+
+    #[test]
+    fn negative_int_literal() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new().parse(&mut errors, "-200");
+        assert!(expr.is_ok());
+        assert_eq!(&format!("{:?}", expr.unwrap()), "-200");
+    }
+
+    #[test]
+    fn float() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new().parse(&mut errors, "-13.37");
+        assert!(expr.is_ok());
+        assert_eq!(&format!("{:?}", expr.unwrap()), "-13.37")
+    }
+
+    #[test]
+    fn float_exponent() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new().parse(&mut errors, "-20.6e20");
+        assert!(expr.is_ok());
+        assert_eq!(&format!("{:?}", expr.unwrap()), "-2060000000000000000000");
+    }
+
+    #[test]
+    fn float_exponent_without_dot() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new().parse(&mut errors, "-20.6e20");
+        assert!(expr.is_ok());
+        assert_eq!(&format!("{:?}", expr.unwrap()), "-2060000000000000000000");
+    }
+
+    #[test]
+    fn hex_int() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new().parse(&mut errors, "0x70Aa");
+        assert!(expr.is_ok());
+        assert_eq!(&format!("{:?}", expr.unwrap()), "28842");
+    }
+
+    #[test]
+    fn function_call() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new().parse(&mut errors, "test_function(one, two)");
+        assert!(expr.is_ok());
+        assert_eq!(&format!("{:?}", expr.unwrap()), "test_function(one, two)");
+    }
+
+    #[test]
+    fn function_call_no_args() {
+        let mut errors = vec![];
+        let expr = main_parser::ExprParser::new().parse(&mut errors, "test_function()");
+        assert!(expr.is_ok());
+        assert_eq!(&format!("{:?}", expr.unwrap()), "test_function()");
+    }
 }
